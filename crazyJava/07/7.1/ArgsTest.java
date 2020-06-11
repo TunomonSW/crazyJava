1f0f894d314202849cbd12d632472df957c8b20f
温欣亚 <837582050@qq.com>
2020-06-11 15:55:27 +0800

    no message

--- crazyJava/07/7.1/ArgsTest.java
+++ crazyJava/07/7.1/ArgsTest.java
@@ -14,9 +14,9 @@ public class ArgsTest
 {
 	public static void main(String[] args)
 	{
-		// 输出args数组的长度
+		//  输出args数组的长度
 		System.out.println(args.length);
-		// 遍历args数组的每个元素
+		//  遍历args数组的每个元素
 		for (String arg : args)
 		{
 			System.out.println(arg);
