332801d8317250a6a743177d7cb802a538a24c6d
wenxinya <837582050@qq.com>
2021-04-23 09:37:13 +0800

    优化

--- crazyJava/05/5.1/Person.java
+++ crazyJava/05/5.1/Person.java
@@ -7,17 +7,17 @@
  * This program is protected by copyright laws.<br>
  * Program Name:<br>
  * Date:<br>
+ *
  * @author Yeeku.H.Lee kongyeeku@163.com
  * @version 1.0
  */
-public class Person
-{
-	// 下面定义了两个成员变量
-	public String name;
-	public int age;
-	// 下面定义了一个say方法
-	public void say(String content)
-	{
-		System.out.println(content);
-	}
+public class Person {
+    // 下面定义了两个成员变量
+    public String name;
+    public int age;
+
+    // 下面定义了一个say方法
+    public void say(String content) {
+        System.out.println(content);
+    }
 }
