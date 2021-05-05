623abe9181c154a821452bc9541ede96630d2dae
wenxinya <837582050@qq.com>
2021-05-05 21:14:27 +0800

    优化

--- crazyJava/05/5.1/Dog.java
+++ crazyJava/05/5.1/Dog.java
@@ -21,8 +21,10 @@ public class Dog
 	// 定义一个run()方法，run()方法需要借助jump()方法
 	public void run()
 	{
+
 //		Dog d = new Dog();
 //		d.jump();
+
 		// 使用this引用调用run()方法的对象
 		this.jump();
 		System.out.println("正在执行run方法");
