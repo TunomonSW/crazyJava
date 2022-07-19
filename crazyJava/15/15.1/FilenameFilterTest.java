17858dd6c114ab8c08f110ed7a9da3d8bf9647a3
wenxinya <837582050@qq.com>
2022-07-19 20:35:03 +0800

    代码逻辑优化

--- crazyJava/15/15.1/FilenameFilterTest.java
+++ crazyJava/15/15.1/FilenameFilterTest.java
@@ -19,6 +19,7 @@ public class FilenameFilterTest
 		// 如果文件名以.java结尾，或者文件对应一个路径，返回true
 		String[] nameList = file.list((dir, name) -> name.endsWith(".java")
 			|| new File(name).isDirectory());
+		assert nameList != null;
 		for(String name : nameList)
 		{
 			System.out.println(name);
