b92282ce717a200b9f560c022d606977783c1753
wenxinya <837582050@qq.com>
2022-09-01 13:00:03 +0800

    代码优化

--- crazyJava/16/16.2/FirstThread.java
+++ crazyJava/16/16.2/FirstThread.java
@@ -22,7 +22,7 @@ public class FirstThread extends Thread
 			// 当线程类继承Thread类时，直接使用this即可获取当前线程
 			// Thread对象的getName()返回当前该线程的名字
 			// 因此可以直接调用getName()方法返回当前线程的名
-			System.out.println(getName() +  " " + i);
+			System.out.println(getName() +  "-" + i);
 		}
 	}
 	
@@ -32,7 +32,7 @@ public class FirstThread extends Thread
 		{
 			// 调用Thread的currentThread方法获取当前线程
 			System.out.println(Thread.currentThread().getName()
-				+  " " + i);
+				+  "-" + i);
 			if (i == 20)
 			{
 				// 创建、并启动第一条线程
