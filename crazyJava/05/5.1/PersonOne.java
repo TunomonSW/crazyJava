f348030433279904623d88670bf6ab5657eda070
wenxinya <837582050@qq.com>
2023-05-10 21:23:23 +0800

    代码优化

--- crazyJava/05/5.1/PersonOne.java
+++ crazyJava/05/5.1/PersonOne.java
@@ -14,7 +14,8 @@
 public class PersonOne {
     // 下面定义了两个成员变量
     public String name;
-    public int age;
+
+//    public int age;
     // 下面定义了一个say方法
     public void say(String content) {
         System.out.println(content);
