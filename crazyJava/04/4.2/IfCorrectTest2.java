1f7777c1c4ea768cf3ad4220a7262b4269ccf0cd
xinyawen <837582050@qq.com>
2023-06-06 12:35:57 +0800

    Update IfCorrectTest2.java
    格式优化

--- crazyJava/04/4.2/IfCorrectTest2.java
+++ crazyJava/04/4.2/IfCorrectTest2.java
@@ -19,11 +19,13 @@ public class IfCorrectTest2
 		{
 			System.out.println("老年人");
 		}
+		
 		// 在原本的if条件中增加了else的隐含条件
 		if (age > 40 && !(age >60))
 		{
 			System.out.println("中年人");
 		}
+		
 		// 在原本的if条件中增加了else的隐含条件
 		if (age > 20 && !(age > 60) && !(age > 40 && !(age >60)))
 		{
